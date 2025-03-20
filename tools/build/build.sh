77395fd6e3fa59ad2434ae44c8601f7b65aa1c68
pollenjp <polleninjp@gmail.com>
2025-03-20 13:51:56 +0900

    use stable for build

--- tools/build/build.sh
+++ tools/build/build.sh
@@ -20,9 +20,7 @@ set -eux -o pipefail
 CARGO_DIST_GLIB_VERSION=${CARGO_DIST_GLIB_VERSION:-2.17}
 
 # Install Rust toolchain
-# rustup update --no-self-update stable
-rustup install 1.85.0
-rustup default 1.85.0
+rustup update --no-self-update stable
 rustup target add "${BUILD_BINARY_TARGET}"
 rustup component add rust-src
 
